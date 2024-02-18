ed92abcaca08c351fe01fa9db00ed2b98b32cf10
Dakshin Krishnan <dakshinxo@gmail.com>
2024-02-18 11:30:04 +0530

    final commit

--- handlers/fishingTrip.js
+++ handlers/fishingTrip.js
@@ -6,7 +6,7 @@ async function tripEndHandler(phoneNumber) {
         tripstatus: 1
     },
         {
-            $set: 0
+            $set: { tripstatus: 0 }
         });
 }
 
@@ -143,11 +143,11 @@ const handleIndividualTripAlert = async (req, res) => {
 
 const handleFishingZoneSuggestionPing = async (parsedMessage) => {
     try {
-        const {  lat, long ,phonenumber} = parsedMessage;
+        const { lat, long, phonenumber } = parsedMessage;
         const suggestionRequest = new FishingSuggestion({
             lat: lat,
             long: long,
-            phonenumber : phonenumber
+            phonenumber: phonenumber
         });
         await suggestionRequest.save();
         return;
@@ -158,39 +158,78 @@ const handleFishingZoneSuggestionPing = async (parsedMessage) => {
 }
 
 const handleFishingZoneSuggestionAlert = async (req, res) => {
-    
-    try{
-    const { lat, long, phonenumber} = req.body;
 
-    client.publish("smser", JSON.stringify({
-        "from": "+918925423535",
-        "to": `+91${phonenumber}`,
-        "msg": `SR ${lat} ${long}`
-    }), function (err) {
-        if (err) {
-            console.error("Error occurred while publishing message:", err);
-            return res.status(201).send({ message: "Cannot send message" });
-        } else {
-            console.log("Message published successfully");
-            return res.status(200).send({ message: "Message sent successfully" });
-        }
-    })}
-    catch(err){
+    try {
+        const { lat, long, phonenumber } = req.body;
+
+        const fishingSuggestion = await FishingSuggestion.findOne({
+            phonenumber: phonenumber
+        })
+
+        const slat = parseFloat(fishingSuggestion.lat);
+        const slong = parseFloat(fishingSuggestion.long);
+
+        const elat = parseFloat(lat);
+        const elong = parseFloat(long);
+
+        const haversineDistance = haversine(slat, slong, elat, elong);
+        const bearingCalculated = bearing(slat, slong, elat, elong);
+
+        client.publish("smser", JSON.stringify({
+            "from": "+918925423535",
+            "to": `+91${phonenumber}`,
+            "msg": `SR ${lat} ${long} ${haversineDistance} ${bearingCalculated}`
+        }), function (err) {
+            if (err) {
+                console.error("Error occurred while publishing message:", err);
+                return res.status(201).send({ message: "Cannot send message" });
+            } else {
+                console.log("Message published successfully");
+                return res.status(200).send({ message: "Message sent successfully" });
+            }
+        })
+    }
+    catch (err) {
         console.error(err);
-        return res.status(200).send({message : "Couldnt send message"});
+        return res.status(200).send({ message: "Couldnt send message" });
     }
 
 }
 
-const getAllSuggestionPings= async(req,res) =>{
-    try{
+const getAllSuggestionPings = async (req, res) => {
+    try {
         const allSuggestionData = await FishingSuggestion.find();
-        return res.status(200).json({suggestionData : allSuggestionData});
-    }catch(err){
+        return res.status(200).json({ suggestionData: allSuggestionData });
+    } catch (err) {
         console.error(err);
-        return res.status(200).send({message : "Couldnt retreive suggestion pings"});
+        return res.status(200).send({ message: "Couldnt retreive suggestion pings" });
     }
 }
 
+function haversine(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var distance = R * c;
+    distance = distance.toString()
+    return distance;
+}
 
-module.exports = { getAllSuggestionPings, handleCaptureUpdate, handleLocationUpdate, tripStartHandler, tripEndHandler, handleIndividualTripAlert, handleFishingZoneSuggestionPing , handleFishingZoneSuggestionAlert};
+function bearing(lat1, lon1, lat2, lon2) {
+    const dLon = (lon2 - lon1) * Math.PI / 180;
+    const y = Math.sin(dLon) * Math.cos(lat2 * Math.PI / 180);
+    const x = Math.cos(lat1 * Math.PI / 180) * Math.sin(lat2 * Math.PI / 180) -
+        Math.sin(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.cos(dLon);
+    let brng = Math.atan2(y, x);
+    brng = brng * 180 / Math.PI;
+    brng = (brng + 360) % 360;
+    brng = brng.toString();
+    return brng;
+}
+
+module.exports = { getAllSuggestionPings, handleCaptureUpdate, handleLocationUpdate, tripStartHandler, tripEndHandler, handleIndividualTripAlert, handleFishingZoneSuggestionPing, handleFishingZoneSuggestionAlert };
